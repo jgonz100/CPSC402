f9920121741ee5889980903da4a334b4e938e79b
jgonz100 <52713480+jgonz100@users.noreply.github.com>
2020-02-11 18:17:13 -0800

    Add files via upload

---
+++ Assignment 1/specification3.py
@@ -0,0 +1,31 @@
+with open("specification3.txt") as f:
+    state = 0
+    i = 1
+    matches = []
+    for line in f:
+        for c in line:
+            if state == 0 and c == 'b':
+                state = 0
+            elif state == 0 and c == 'a':
+                state = 1
+            elif state == 1 and c == 'a':
+                state = 2
+                matches.append(i)
+            elif state == 2 and c == 'b':
+                state = 3
+            elif state == 2 and c == 'a':
+                state = 2
+                matches.append(i)
+            elif state == 2 and c == 'b':
+                state = 3
+            elif state == 3 and c == 'a':
+                state = 1
+            elif state == 3 and c == 'b':
+                state = 4
+                matches.append(i)
+            elif state == 4 and c == 'a':
+                state = 1
+            elif state == 4 and c == 'b':
+                state = 0
+            i += 1
+    print(matches)
