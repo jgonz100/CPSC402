756155e65baa8213a8accf4092d8991442bd7f8e
jgonz100 <52713480+jgonz100@users.noreply.github.com>
2020-02-12 11:22:29 -0800

    Add files via upload

---
+++ Assignment 1/main.py
@@ -0,0 +1,10 @@
+import solutions.specification1
+import solutions.specification3
+
+def main():
+    solutions.specification1.run_spec1()
+    solutions.specification3.run_spec3()
+    
+if __name__ == '__main__':
+    main()
+    
---
+++ Assignment 1/solutions/specification1.py
@@ -0,0 +1,39 @@
+# CPSC 402, Dr. Alexander Kurz
+# Jadyn Gonzalez and Michelle Kutsanov
+
+# FSM based on the matching pattern abab
+def run_spec1():
+    with open("specification1.txt") as f:
+        state = 0
+        i = 1
+        matches = []
+        for line in f:  
+           for c in line:
+               if state == 0 and (c == 'b' or c == 'c'):
+                   state = 0
+               elif state == 0 and c == 'a':
+                   state = 1
+               elif state == 1 and c == 'a':
+                   state = 1
+               elif state == 1 and c == 'c':
+                   state = 0
+               elif state == 1 and c == 'b':
+                   state = 2
+               elif state == 2 and (c == 'b' or c == 'c'):
+                   state = 0
+               elif state == 2 and c == 'a':
+                   state = 3
+               elif state == 3 and c == 'a':
+                   state = 1
+               elif state == 3 and c == 'c':
+                   state = 0
+               elif state == 3 and c == 'b':
+                   state = 4
+                   matches.append(i)
+               elif state == 4 and (c == 'b' or c == 'c'):
+                   state = 0
+               elif state == 4 and c == 'a':
+                   state = 3
+               i+=1
+        print(matches)
+
