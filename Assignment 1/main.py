bdc3b4ea72a0c0ce11b98077ba28e80d0c9a9849
jgonz100 <52713480+jgonz100@users.noreply.github.com>
2020-02-13 15:51:07 -0800

    Update main.py

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -3,8 +3,13 @@ import solutions.specification2
 import solutions.specification3
 
 def main():
+    print("Match indices for specification 1:")
     solutions.specification1.run_spec1()
+    print()
+    print("Match indices for specification 2:")
     solutions.specification2.run_spec2()
+    print()
+    print("Match indices for specification 3:")
     solutions.specification3.run_spec3()
     
 if __name__ == '__main__':
